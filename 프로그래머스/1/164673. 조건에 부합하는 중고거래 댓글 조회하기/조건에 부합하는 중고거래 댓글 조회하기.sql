e75b55f8afc71366caac8f136e9642855f21e6da
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-04-02 12:42:32 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT TITLE, b.BOARD_ID, REPLY_ID, r.WRITER_ID, r.CONTENTS, to_char(r.CREATED_DATE,'yyyy-mm-dd') from USED_GOODS_REPLY r, USED_GOODS_BOARD b where r.BOARD_ID = b.BOARD_ID and to_char(b.CREATED_DATE,'yyyy-mm') in ('2022-10') order by r.CREATED_DATE, TITLE;
