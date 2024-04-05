6964a4ba93fa8fe755dcbd0777c4f3929fd44671
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-04-05 11:17:23 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, decode(STATUS,'SALE','판매중','RESERVED','예약중','DONE','거래완료') as STATUS from USED_GOODS_BOARD where to_char(CREATED_DATE, 'yyyy-mm-dd') = '2022-10-05' order by BOARD_ID desc
