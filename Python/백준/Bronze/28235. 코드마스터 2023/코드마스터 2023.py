5bcf1ea3b02cc7c8944057dde0fbe051bc2953e5
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-12 22:41:11 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+string = input()
+
+if string == "SONGDO":
+    print('HIGHSCHOOL')
+elif string == 'CODE':
+    print('MASTER')
+elif string == '2023':
+    print('0611')
+else:
+    print('CONTEST')
