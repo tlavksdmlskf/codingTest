8edc748b9778cdbeb6f1fc0a229951b2f266de4c
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-16 00:37:46 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,17 @@
+wave = int(input())
+
+if wave < 425:
+    print("Violet")
+elif wave < 450:
+    print("Indigo")
+elif wave < 495:
+    print("Blue")
+elif wave <  570:
+    print("Green")
+elif wave < 590:
+    print("Yellow")
+elif wave < 620:
+    print("Orange")
+else:
+    print("Red")
+
