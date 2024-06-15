d2fc042e0b3fe2f16dc96156e5ac2f1e6a914a64
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-15 23:28:24 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+string = input()
+
+if string == 'M':
+    print('MatKor')
+elif string == 'W':
+    print('WiCys')
+elif string == 'C':
+    print('CyKor')
+elif string == 'A':
+    print('AlKor')
+else:
+    print('$clear')
