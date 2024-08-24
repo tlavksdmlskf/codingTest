91d52f026404ba942eb7d7cce505582dcc26cacb
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-08-24 19:26:38 +0900

    [Bronze IV] Title: Tournament Selection, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/14038. Tournament Selection/Tournament Selection.py
@@ -0,0 +1,14 @@
+cnt = 0
+
+for _ in range(6):
+    if input() == "W":
+        cnt += 1
+
+if cnt >= 5:
+    print(1)
+elif cnt >= 3:
+    print(2)
+elif cnt >= 1:
+    print(3)
+else:
+    print(-1)
